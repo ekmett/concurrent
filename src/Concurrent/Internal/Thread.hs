7e6a15c878b7f27f9cff2dc91885b5e91a978305
Edward Kmett <ekmett@gmail.com>
2015-10-28 16:21:16 -0400

    separate retrieve of the current capability. add a custom prim for fetching thread id so we can use prim marshalling instead

--- src/Concurrent/Internal/Thread.hs
+++ src/Concurrent/Internal/Thread.hs
@@ -5,33 +5,29 @@
 {-# LANGUAGE GHCForeignImportPrim #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Concurrent.Internal.Thread
-  ( rawThreadId
-  , withCapability
+  ( withCapability
+  , currentThread
+  , currentCapability
   ) where
 
 import Concurrent.Primitive.Class
 import Control.Monad
 import Control.Monad.Catch
 import Control.Monad.Primitive
-import Foreign.C.Types
-import GHC.Conc.Sync
 import GHC.Prim
 import GHC.Exts
 
-foreign import ccall unsafe "rts_getThreadId" rts_getThreadId :: ThreadId# -> CInt
-foreign import prim "pinThreadzh"   pinThread#   :: State# s -> (# State# s, Int#, Int# #)
+foreign import prim "pinThreadzh"   pinThread#   :: State# s -> (# State# s, Int# #)
 -- The funny result type is used to hack around <https://ghc.haskell.org/trac/ghc/ticket/11032>
 foreign import prim "unpinThreadzh" unpinThread# :: State# s -> (# State# s #)
-
-rawThreadId :: ThreadId -> CInt
-rawThreadId (ThreadId x) = rts_getThreadId x
-{-# INLINE rawThreadId #-}
+foreign import prim "currentThreadzh" currentThread# :: State# s -> (# State# s, Int# #)
+foreign import prim "currentCapabilityzh" currentCapability# :: State# s -> (# State# s, Int# #)
 
 -- | Pin the current thread to whatever capability is executing it.
 -- This returns the capability we are pinned to and if we were previously pinned.
-pinThread :: MonadPrimIO m => m (Bool, Int)
+pinThread :: MonadPrimIO m => m Bool
 pinThread = primitive $ \s -> case pinThread# s of
-  (# s', b, c #) -> (# s', (isTrue# b, I# c) #)
+  (# s', b #) -> (# s', isTrue# b #)
 {-# INLINE pinThread #-}
 
 unpinThread :: MonadPrimIO m => Bool -> m ()
@@ -42,5 +38,18 @@ unpinThread b = unless b $ primitive $ \s -> case unpinThread# s of
 -- | Pin the current thread to its capability and run a computation.
 --
 -- This lets us use "capability-local" variables and the like.
-withCapability :: (MonadMask m, MonadPrimIO m) => (Int -> m a) -> m a
-withCapability f = bracket pinThread (unpinThread . fst) (f . snd)
+withCapability :: (MonadMask m, MonadPrimIO m) => m a -> m a
+withCapability m = bracket pinThread unpinThread (const m)
+{-# INLINE withCapability #-}
+
+-- | Retrieve the current thread id
+currentThread :: PrimMonad m => m Int
+currentThread = primitive $ \s -> case currentThread# s of
+  (# s', i #) -> (# s', I# i #)
+{-# INLINE currentThread #-}
+
+-- | Retrieve the current capability id
+currentCapability :: PrimMonad m => m Int
+currentCapability = primitive $ \s -> case currentCapability# s of
+  (# s', i #) -> (# s', I# i #)
+{-# INLINE currentCapability #-}
