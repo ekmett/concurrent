c3099a62c31a0df81cbb495a615b8e2870d731c5
Edward Kmett <ekmett@gmail.com>
2015-10-28 18:50:19 -0400

    Add primST

--- src/Concurrent/Primitive/Class.hs
+++ src/Concurrent/Primitive/Class.hs
@@ -7,6 +7,7 @@
 module Concurrent.Primitive.Class where
 
 import Control.Monad.Primitive
+import Control.Monad.ST
 
 class (PrimMonad m, PrimState m ~ s) => MonadPrim s m | s -> m
 instance (PrimMonad m, PrimState m ~ s) => MonadPrim s m
@@ -17,3 +18,7 @@ instance MonadPrim RealWorld m => MonadPrimIO m
 primIO :: MonadPrimIO m => IO a -> m a 
 primIO = primToPrim
 {-# INLINE primIO #-}
+
+primST :: PrimMonad m => ST (PrimState m) a -> m a
+primST = primToPrim
+{-# INLINE primST #-}
