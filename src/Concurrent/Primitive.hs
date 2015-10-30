ca11d5f34a7fbdbb8c0a5b7d559c1d733ca066ba
Edward Kmett <ekmett@gmail.com>
2015-10-29 20:47:29 -0400

    Consolidated all array modules into one. Prerequisitefor SmallArrayArrays

--- src/Concurrent/Primitive.hs
+++ src/Concurrent/Primitive.hs
@@ -1,19 +1,15 @@
 {-# LANGUAGE CPP #-}
 module Concurrent.Primitive 
   ( module Concurrent.Primitive.Array
-  , module Concurrent.Primitive.ArrayArray
   , module Concurrent.Primitive.Class
   , module Concurrent.Primitive.MVar
   , module Concurrent.Primitive.PrimRef
-  , module Concurrent.Primitive.SmallArray
   )where
 
 import Concurrent.Primitive.Array
-import Concurrent.Primitive.ArrayArray
 import Concurrent.Primitive.Class
 import Concurrent.Primitive.MVar
 import Concurrent.Primitive.PrimRef
-import Concurrent.Primitive.SmallArray
 
 #ifdef HLINT
 {-# ANN module "HLint: ignore Use import/export shortcut" #-}
